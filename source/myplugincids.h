169e49bc77832b1085a2ec776a70d0d10589cddf
Timothy Graupmann <tim.graupmann@razer.com>
2021-01-16 10:59:30 -0800

    Initial commit of project

---
+++ source/myplugincids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2021 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kVST3_ChromaSDKPluginProcessorUID (0xAA68CE00, 0xC5C856A8, 0xB8744C58, 0x188677FA);
+static const Steinberg::FUID kVST3_ChromaSDKPluginControllerUID (0x8D9FFE1B, 0xD43C574C, 0x995C1442, 0x35D67CA5);
+
+#define VST3_ChromaSDKPluginVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
---
+++ source/mypluginentry.cpp
@@ -0,0 +1,71 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2021 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#include "mypluginprocessor.h"
+#include "myplugincontroller.h"
+#include "myplugincids.h"
+#include "version.h"
+
+#include "public.sdk/source/main/pluginfactory.h"
+
+#define stringPluginName "VST3_ChromaSDKPlugin"
+
+//------------------------------------------------------------------------
+//  Module init/exit
+//------------------------------------------------------------------------
+
+//------------------------------------------------------------------------
+// called after library was loaded
+bool InitModule ()
+{
+	return true;
+}
+
+//------------------------------------------------------------------------
+// called after library is unloaded
+bool DeinitModule ()
+{
+	return true;
+}
+
+using namespace Steinberg::Vst;
+using namespace MyCompanyName;
+
+//------------------------------------------------------------------------
+//  VST Plug-in Entry
+//------------------------------------------------------------------------
+// Windows: do not forget to include a .def file in your project to export
+// GetPluginFactory function!
+//------------------------------------------------------------------------
+
+BEGIN_FACTORY_DEF ("My Plug-in Company", 
+			       "https://www.mycompanyname.com", 
+			       "mailto:info@mycompanyname.com")
+
+	//---First Plug-in included in this factory-------
+	// its kVstAudioEffectClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(kVST3_ChromaSDKPluginProcessorUID),
+				PClassInfo::kManyInstances,	// cardinality
+				kVstAudioEffectClass,	// the component category (do not changed this)
+				stringPluginName,		// here the Plug-in name (to be changed)
+				Vst::kDistributable,	// means that component and controller could be distributed on different computers
+				VST3_ChromaSDKPluginVST3Category, // Subcategory for this Plug-in (to be changed)
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				VST3_ChromaSDKPluginProcessor::createInstance)	// function pointer called when this component should be instantiated
+
+	// its kVstComponentControllerClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID (kVST3_ChromaSDKPluginControllerUID),
+				PClassInfo::kManyInstances, // cardinality
+				kVstComponentControllerClass,// the Controller category (do not changed this)
+				stringPluginName "Controller",	// controller name (could be the same than component name)
+				0,						// not used here
+				"",						// not used here
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				VST3_ChromaSDKPluginController::createInstance)// function pointer called when this component should be instantiated
+
+	//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
+
+END_FACTORY
